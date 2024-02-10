18721a7955869b49722c7019cd4fff3d9c5c7a26
小逸 <2429016980.com>
2024-02-10 09:43:21 +0800

    图片懒加载

--- src/directives/index.js
+++ src/directives/index.js
@@ -1,21 +1,22 @@
 // 定义懒加载插件
-import { useIntersectionObserver } from '@vueuse/core'
+console.log('防止报错')
+// import { useIntersectionObserver } from '@vueuse/core'
 
-export const lazyPlugin = {
-  install(app) {
-    app.directive('img-lazy', {
-      mounted(el, binding) {
-        // el: 指令绑定的那个元素 img
-        // binding: binding.value  指令等于号后面绑定的表达式的值  图片url
-        console.log(el, binding.value)
-        const { stop } = useIntersectionObserver(el, ([{ isIntersecting }]) => {
-          if (isIntersecting) {
-            //  进入视口区域
-            el.scr = binding.value
-            stop()
-          }
-        })
-      }
-    })
-  }
-}
+// export const lazyPlugin = {
+//   install(app) {
+//     app.directive('img-lazy', {
+//       mounted(el, binding) {
+//         // el: 指令绑定的那个元素 img
+//         // binding: binding.value  指令等于号后面绑定的表达式的值  图片url
+//         console.log(el, binding.value)
+//         const { stop } = useIntersectionObserver(el, ([{ isIntersecting }]) => {
+//           if (isIntersecting) {
+//             // 进入视口区域
+//             el.src = binding.value
+//             stop()
+//           }
+//         })
+//       }
+//     })
+//   }
+// }
--- src/main.js
+++ src/main.js
@@ -6,11 +6,13 @@ import router from './router'
 import 'element-plus/theme-chalk/dark/css-vars.css'
 import './styles/dark/css-vars.css'
 
+import { useIntersectionObserver } from '@vueuse/core'
+
 // 引入初始化样式文件
 import '@/styles/common.scss'
 
 // 引入懒加载指令插件并注册
-import { lazyPlugin } from '@/directives'
+// import { lazyPlugin } from '@/directives'
 
 const app = createApp(App)
 
@@ -19,5 +21,20 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 // app.use(VueLazyload)
-app.use(lazyPlugin)
+// app.use(lazyPlugin)
 app.mount('#app')
+
+// 定义全局指令
+app.directive('img-lazy', {
+  mounted(el, binding) {
+    console.log(el, binding.value)
+    useIntersectionObserver(el, ([{ isIntersecting }]) => {
+      console.log(isIntersecting)
+
+      if (isIntersecting) {
+        // 进入视口区域
+        el.src = binding.value
+      }
+    })
+  }
+})
